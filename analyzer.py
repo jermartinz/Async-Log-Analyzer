9f918a3c48aa4fa4975e0a646e73010341403978
Jeremy A. <61643086+jermartinz@users.noreply.github.com>
2025-09-13 01:49:45 -0300

    feat: add basic log parsing functionality

--- analyzer.py
+++ analyzer.py
@@ -1,1 +1,52 @@
+import asyncio
+import aiofiles
 
+
+
+async def file_analysis(log_files):
+    count_event = {"INFO": 0, "WARN": 0, "ERROR": 0, "DEBUG": 0}
+    first_error = None
+    last_error = None
+    max_warn_consecutives = 0
+    current_warn = 0
+    
+    async with aiofiles.open(log_files, 'r', encoding='utf-8') as file:
+        async for line in file:
+            sections = line.strip().split(' ', 3)
+            if len(sections) >= 4:
+                _, _, level, _ = sections
+                
+                if level in count_event:
+                    count_event[level] += 1
+                
+                # Lógica de errores
+                if level == "ERROR":
+                    complete_line = line.strip()
+                    if first_error is None:
+                        first_error = complete_line
+                    last_error = complete_line
+                    current_warn = 0
+                
+                
+                elif level == "WARN":
+                    current_warn += 1
+                    if current_warn > max_warn_consecutives:
+                        max_warn_consecutives = current_warn
+                else:
+                    current_warn = 0
+                
+                if count_event["INFO"] % 100 == 0:
+                    await asyncio.sleep(0)
+    
+    return {
+        'file': log_files,
+        'counts': count_event,
+        'firs_error': first_error,
+        'last_error': last_error,
+        'max_warn_consecutives': max_warn_consecutives
+    }
+
+async def multi_file_analysis(log_files):
+    tasks = [file_analysis(log_file) for log_file in log_files]
+    results = await asyncio.gather(*tasks)
+    return results
--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import sys
-
+import asyncio
 try:
-    import parser_log as pl
+    import analyzer as an
 except ImportError as e:
     print(f"Error importing module: {e}")
     sys.exit(1)
 
-def main():
-    log_file_path = "sample.log"
-    lines = pl.parser_line(log_file_path)
-    for date, hour, level, message in lines:
-        print(f"Date: {date}, Hour: {hour}, Level: {level}, Message: {message}")
-    events = pl.count_events(log_file_path)
-    for level, total in events.most_common():
-        print(f"Level: {level}, Count: {total}")
+async def main():
+    result = await an.file_analysis('sample.log')
+    print(f"File: {result['file']}")
+    print(f"Counts: {result['counts']}")
+
+    files = ['sample.log', 
+            'sample_1.log', 'sample_2.log', 
+            'sample_3.log']
+    
+    multi_results = await an.multi_file_analysis(files)
+    for res in multi_results:
+        print(f"File: {res['file']}")
+        print(f"Counts: {res['counts']}")
+        print(f"First Error: {res['firs_error']}")
+        print(f"Last Error: {res['last_error']}")
+        print(f"Max Consecutive WARN: {res['max_warn_consecutives']}")
+        print("-----")
+
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
